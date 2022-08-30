7a1790c25483b7abf2f6caa730dbe89ff8e24ba8
Conor Fleming <conorfleming03@gmail.com>
2022-08-29 22:31:15 -0700

    working on drawing some lines

--- window.py
+++ window.py
@@ -1,4 +1,6 @@
+from textwrap import fill
 from tkinter import Tk, BOTH, Canvas
+from turtle import width
 
 class Window:
     def __init__(self, width, height) -> None:
@@ -12,6 +14,11 @@ class Window:
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
+
+    def draw_line(self, Line, fill_color):
+        Line.draw(Line, fill_color)
+
+
     
     def wait_for_close(self):
         self.__running = True
@@ -21,8 +28,29 @@ class Window:
     def close(self):
         self.__root = False
 
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, Point1, Point2) -> None:
+        self.__point1 = Point1
+        self.__point2 = Point2
+
+#working on the canvas item here
+    def draw(self, Canvas, fill_color):
+        .create_line(self.__point1, self.__point2, fill = fill_color, width = 2)
+        .pack(fill = BOTH, expand = True)
+
+        
+
 def main():
     win = Window(800, 600)
+    line1 = Line(Point(3, 6),Point(7, 10))
+    line2 = Line( Point(1, 8),Point(7, 2))
+    win.draw_line(line1, "green")
+    win.draw_line(line2, "red")
     win.wait_for_close()
 
 main()
