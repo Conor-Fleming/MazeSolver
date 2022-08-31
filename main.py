73be99d153350662d104f14a1c1c97dac3a20e04
Conor Fleming <conorfleming03@gmail.com>
2022-08-30 22:19:38 -0700

    working on drawing cells for maze, program running breifly but not getting window

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from window import Window, Line, Point
+from window import Cell, Window, Line, Point
 
 def main():
     win = Window(800, 600)
-    line1 = Line(Point(30, 60),Point(75, 400))
-    line2 = Line( Point(100, 80),Point(7, 200))
-    win.draw_line(line1, "green")
-    win.draw_line(line2, "red")
-    win.wait_for_close()
+    
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(10, 10, 100, 100)
+
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(10, 10, 100, 100)
+
+    cell = Cell(win)
+    cell.has_bottom_wall = False
+    cell.draw(100, 100, 50, 50)
+
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(20, 10, 20, 10)
+
+    win.wait_for_close
 
 main()
--- window.py
+++ window.py
@@ -1,3 +1,5 @@
+from ctypes.wintypes import POINT
+from curses import window
 from textwrap import fill
 from tkinter import Tk, BOTH, Canvas
 from turtle import width
@@ -26,18 +28,46 @@ class Window:
     def close(self):
         self.__running = False
 
-class Point:
-    def __init__(self, x, y) -> None:
-        self.x = x
-        self.y = y
+class Cell:
+    def __init__(self, window) -> None:
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.x1 = None
+        self.x2 = None
+        self.y1 = None
+        self.y2 = None
+        self.window = window
+
+    def draw(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        if self.has_left_wall:
+            border = Line(Point(x1, y1), Point(x1, y2))
+            self.window.draw_line(border, "black")
+        if self.has_right_wall:
+            border = Line(Point(x2, y1), Point(x2, y2))
+            self.window.draw_line(border, "black")
+        if self.has_top_wall:
+            border = Line(Point(x1, y1), Point(x2, y1))
+            self.window.draw_line(border, "black")
+        if self.has_bottom_wall:
+            border = Line(Point(x1, y2), Point(x2, y2))
+            self.window.draw_line(border, "black")
 
 class Line:
     def __init__(self, point1, point2) -> None:
         self.point1 = point1
         self.point2 = point2
 
-#working on the canvas item here
     def draw(self, canvas, fill_color):
         canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill = fill_color, width = 2)
         canvas.pack(fill = BOTH, expand = True)
 
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
