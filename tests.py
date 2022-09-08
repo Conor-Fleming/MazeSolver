2d34444bacfbb6c3554daf07524d64cdc1ca64d6
Conor Fleming <conorfleming03@gmail.com>
2022-09-07 20:12:36 -0700

    tests

--- tests.py
+++ tests.py
@@ -3,22 +3,41 @@ from maze import Maze
 
 
 class Test_create_cells(unittest.TestCase):
-    def runTest1(self):
-        maze = Maze(1, 1, 100, 50, 50, 50)
 
-        self.assertEqual(len(maze.cells), maze.columns, "columns is not correct")
-        print(f"{len(maze.cells)} columns")
-        self.assertEqual(len(maze.cells[0]), maze.rows, "rows are not correct")
-        print(f"{len(maze.cells[0])} rows")
+    def setUp(self):
+        pass
+
+    def test_Rows_Cols1(self):
+        rows = 100
+        cols = 50
+        maze1 = Maze(0, 0, rows, cols, 50, 50)
+        
+        self.assertEqual(len(maze1.cells), maze1.columns, "columns is not correct")
+       
+        self.assertEqual(len(maze1.cells[0]), maze1.rows, "rows are not correct")
+       
 
 
-    def runTest2(self):
-        maze = Maze(1, 1, 4, 77, 50, 50)
+    def test_Rows_Cols2(self):
+        rows = 4
+        cols = 77
+        maze2 = Maze(0, 0, rows, cols, 50, 50)
 
-        self.assertEqual(len(maze.cells), maze.columns, "columns is not correct")
-        print(f"{len(maze.cells)} columns")
-        self.assertEqual(len(maze.cells[0]), maze.rows, "rows are not correct")
-        print(f"{len(maze.cells[0])} rows")
+        self.assertEqual(len(maze2.cells), maze2.columns, "columns is not correct")
+  
+        self.assertEqual(len(maze2.cells[0]), maze2.rows, "rows are not correct")
+       
 
-if __name__ == '__main__':
+    def test_Rows_Cols3(self):
+        rows = 3
+        cols = 2
+        maze3 = Maze(0, 0, rows, cols, 50, 50)
+
+        self.assertEqual(len(maze3.cells), maze3.columns, "columns is not correct")
+    
+        self.assertEqual(len(maze3.cells[0]), maze3.rows, "rows are not correct")
+       
+
+
+if __name__ == "__main__":
     unittest.main() 
