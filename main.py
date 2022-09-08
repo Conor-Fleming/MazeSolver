ed67288513c0007ac6010811a98ff12424621775
Conor Fleming <conorfleming03@gmail.com>
2022-09-07 18:47:57 -0700

    tweaking maze class

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
 from window import Window, Line, Point
 from cell import Cell
+from maze import Maze
 
 def main():
     win = Window(800, 600)
     
-    c0 = Cell(win)
-    c0.has_right_wall = False
-    c0.draw(50, 50, 100, 100)
-
-    c1 = Cell(win)
-    c1.has_left_wall = False
-    c1.has_right_wall = False
-    c1.draw(100, 50, 150, 100)
-
-    c0.draw_move(c1)
-
-    c2 = Cell(win)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(150, 50, 200, 100)
-
-    c1.draw_move(c2)
-
-    c3 = Cell(win)
-    c3.has_top_wall = False
-    c3.has_bottom_wall = False
-    c3.draw(150, 100, 200, 150)
-
-    c2.draw_move(c3)
-
+    maze = Maze(5, 5, 10, 10, 50, 50, win)
+    
     win.wait_for_close()
 
 main()
--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 from cell import Cell
+from window import Window, Line, Point 
 
 
 class Maze:
@@ -11,10 +12,10 @@ class Maze:
         self.cell_sixe_y = cell_size_y
         self.window = window
 
-        self.creat_cells()
+        self.create_cells()
 
-    def create_cells():
-        cells = []
+    def create_cells(self):
+        self.cells = []
 
         for col in self.columns:
             column = []
@@ -22,12 +23,21 @@ class Maze:
                 column.append(Cell(self.window))
             cells.append(column)
         
+        for i in cells:
+            for j in cells[i]:
+                j.draw_cell(i, j)
+        
     #calc x,y of cell with i, j and cell sizes     
     def draw_cell(self, i, j):
-        None
-
+        self.startX = i + self.x1
+        self.startY = j + self.y1
+        self.endX = startX + self.cell_size_x
+        self.endY = startY + self.cell_sixe_y
+        self.draw(self.startX, self.startY, self.endX, self.endY)
+        
     def animate(self):
-        None
+        self.window.redraw()
+        time.sleep(.05)
 
 
 
