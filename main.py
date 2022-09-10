4e3fe7d52a0d80c29a2deb8ca09aaa2cd8cb2be3
Conor Fleming <conorfleming03@gmail.com>
2022-09-10 05:26:25 -0700

    adding requirments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from cell import Cell
 from maze import Maze
 
 def main():
-    win = Window(800, 600)
+    win = Window(525, 525)
     
     maze = Maze(5, 5, 10, 10, 50, 50, win)
     maze.solve()
--- maze.py
+++ maze.py
@@ -39,8 +39,6 @@ class Maze:
             #move to the right
             if col > i and col <= self.columns - 1:
                 if self.cells[col][row].has_left_wall == False and self.cells[col][row].visited == False:
-                    print("right ex")
-                    print(i,j,col,row)
                     self.cells[i][j].draw_move(self.cells[col][row])
                     if self.solve_maze(col, row):
                         return True
@@ -48,8 +46,6 @@ class Maze:
             #move to the left
             if col < i and col >= 0:
                 if self.cells[col][row].has_right_wall == False and self.cells[col][row].visited == False:
-                    print("left ex")
-                    print(i,j,col,row)
                     self.cells[i][j].draw_move(self.cells[col][row])
                     if self.solve_maze(col, row):
                         return True
@@ -57,8 +53,6 @@ class Maze:
             #move down
             if row > j and row <= self.rows - 1:
                 if self.cells[col][row].has_top_wall == False and self.cells[col][row].visited == False:
-                    print("down ex")
-                    print(i,j,col,row)
                     self.cells[i][j].draw_move(self.cells[col][row])
                     if self.solve_maze(col, row):
                         return True
@@ -66,8 +60,6 @@ class Maze:
             #move up
             if row < j and row >= 0:
                 if self.cells[col][row].has_bottom_wall == False and self.cells[col][row].visited == False:
-                    print("up ex")
-                    print(i,j,col,row)
                     self.cells[i][j].draw_move(self.cells[col][row])
                     if self.solve_maze(col, row):
                         return True
